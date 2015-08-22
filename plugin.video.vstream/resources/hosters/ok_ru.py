1c7158d8eba8082bed87c28e236545651df4b4ce
TmpName <temporary.asdsd@moncourrier.fr.nf>
2015-08-22 14:04:57 +0200

    Update ok_ru.py
    Ne marche tjours pas, mais ne marche pas moins.

--- plugin.video.vstream/resources/hosters/ok_ru.py
+++ plugin.video.vstream/resources/hosters/ok_ru.py
@@ -2,8 +2,10 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib.config import cConfig
 from resources.hosters.hoster import iHoster
-import re,urllib2
+import re,urllib2, urllib
 import xbmcgui
+import time
+import random
 
 class cHoster(iHoster):
 
@@ -69,10 +71,43 @@ class cHoster(iHoster):
         sHost = v[0]
         web_url = 'http://' + sHost + '/dk?cmd=videoPlayerMetadata&mid=' + sId
         
+        #bidouille en plus
+        a = int(time.time())
+        b = random.random()
+        web_url = web_url + '&rnd=' + str(a) + str(b)
+        
         #print web_url
         
-        oRequest = cRequestHandler(web_url)
-        sHtmlContent = oRequest.request()
+        UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
+        headers = {'User-Agent': UA ,
+                   'Host' : sHost,
+                   #'Referer': 'http://www.voirfilms.org/batman-unlimited-monstrueuse-pagaille.htm',
+                   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                   'Content-Type': 'text/html; charset=utf-8'}
+        
+        #oRequest = cRequestHandler(web_url)
+        #sHtmlContent = oRequest.request()
+        
+        req = urllib2.Request( 'http://ok.ru/video/' + str(sId),None,headers)
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.URLError, e:
+            print e.read()
+            print e.reason
+        
+        print web_url
+        
+
+        
+        req = urllib2.Request(web_url,None,headers)
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.URLError, e:
+            print e.read()
+            print e.reason
+            
+        sHtmlContent = response.read()
+        response.close()
         
         sHtmlContent = sHtmlContent.decode('unicode-escape')
         sHtmlContent = sHtmlContent.encode("utf-8")
@@ -103,9 +138,20 @@ class cHoster(iHoster):
                 if (ret > -1):
                     api_call = url[ret]
                     
-        #print api_call
-
+        
+        #time.sleep( 5 )        
+                    
+        print api_call
+        #api_call = 'http://217.20.157.207/?sig=1840c53829262bb9b14d50010b73f1722d2ec7e4&ct=0&expires=1439657212454&clientType=0&id=51659082303&type=1'
+        
+        UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'
+        api_call = api_call + '|User-Agent=' + UA
+        
         if (api_call):
-            return True, api_call
+            return True,api_call
             
         return False, False
+
+        
+#http://www.tubeoffline.com/download-Odnoklassniki-videos.php#.Vc4giJf-QZQ
+
