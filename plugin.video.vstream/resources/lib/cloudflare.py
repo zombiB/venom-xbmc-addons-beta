2fcfa191e9353996783581dc7b1520882abb0098
TmpName <temporary.asdsd@moncourrier.fr.nf>
2015-10-18 19:39:08 +0200

    Update cloudflare.py
    A optimiser mais fonctionne

--- plugin.video.vstream/resources/lib/cloudflare.py
+++ plugin.video.vstream/resources/lib/cloudflare.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 #
-import re
+from resources.lib.config import cConfig
+import re,os
 import urllib2,urllib
 import xbmc
 
@@ -12,18 +13,53 @@ def parseInt(chain):
     chain = re.sub(r'\(!\+\[\]','(1',chain)  #si le bloc commence par !+[] >> +1
     chain = re.sub(r'(\([^()]+)\+\[\]\)','(\\1)*10)',chain)  # si le bloc commence par !+[] et fini par +[] >> *10
     
-    #bidouille a optimiser
+    #bidouilles a optimiser non geree encore par regex
     chain = re.sub(r'\(\+\[\]\)','0',chain)
+    if chain.startswith('!+[]'):
+        chain = chain.replace('!+[]','1')
     
     #print chain
-
     return eval(chain)
+    
+    
+class NoRedirection(urllib2.HTTPErrorProcessor):    
+    def http_response(self, request, response):
+        return response
 
 class CloudflareBypass(object):
 
     def __init__(self):
         self.state = False
+                       
+    def DeleteCookie(self,Domain):
+        PathCache = cConfig().getSettingCache()
+        file = os.path.join(PathCache,'Cookie_'+ str(Domain) +'.txt')
+        os.remove(os.path.join(PathCache,file))
+        
+    def SaveCookie(self,Domain,data):
+        PathCache = cConfig().getSettingCache()
+        Name = os.path.join(PathCache,'Cookie_'+ str(Domain) +'.txt')
+
+        #save it
+        file = open(Name,'w')
+        file.write(data)
+
+        file.close()
+        
+    def Readcookie(self,Domain):
+        PathCache = cConfig().getSettingCache()
+        Name = os.path.join(PathCache,'Cookie_'+ str(Domain) +'.txt')
+        
+        try:
+            file = open(Name,'r')
+            data = file.read()
+            file.close()
+        except:
+            return False
+        
+        return data
             
+  
     def check(self,htmlcontent):
         if 'Checking your browser before accessing' in htmlcontent:
             self.state = True
@@ -45,71 +81,96 @@ class CloudflareBypass(object):
         
         return str(rep)
         
-    def Valid(self,url,htmlcontent):
         
-        #test part
-        #url ='http://www.series-en-streaming.tv/saison/250487/1/american-horror-story/'
-        #fh = open('c:\\test.txt', "r")
-        #htmlcontent = fh.read()
-        #fh.close()
+    def Valid(self,url,htmlcontent,cookies):
         
+        self.hostComplet = re.sub(r'(https*:\/\/[^/]+)(\/*.*)','\\1',url)
+        self.host = re.sub(r'https*:\/\/','',self.hostComplet)
+        self.cookies = cookies
         
-        host1  = re.sub(r'(https*:\/\/[^/]+)(\/*.*)','\\1',url)
-        self.host = re.sub(r'https*:\/\/','',host1)
+        self.headers = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'),
+                       ('Host' , self.host),
+                       ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+                       ('Accept-Language','fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3'),
+                       #('Accept-Encoding','gzip, deflate'),
+                       ('Referer', url),
+                       #('Cookie' , self.cookies),
+                       ('Content-Type', 'text/html; charset=utf-8')]
+                       
+                       
+        cookieMem = self.Readcookie(self.host.replace('.','_'))
+        if not (cookieMem == False):
+            cookies = cookieMem
+            print 'cookies present'
+        else:
         
-        #fh = open('c:\\test.txt', "w")
-        #fh.write(htmlcontent)
-        #fh.close()
-        
-        hash = re.findall('<input type="hidden" name="jschl_vc" value="(.+?)"\/>',htmlcontent)[0]
-        passe = re.findall('<input type="hidden" name="pass" value="(.+?)"\/>',htmlcontent)[0]
-        
-        #calcul de la reponse
-        rep = self.GetResponse(htmlcontent)
+            #1 er etape : On recharge la page, pas forcement necessaire mais evite les probleme de headers
+            opener = urllib2.build_opener(NoRedirection)
+            opener.addheaders = self.headers
+            #opener.addheaders.append (('Cookie', self.cookies))
 
-        #xbmc.sleep(5000)
-        
-        NewUrl = host1 + '/cdn-cgi/l/chk_jschl?jschl_vc='+ urllib.quote_plus(hash) +'&pass=' + urllib.quote_plus(passe) + '&jschl_answer=' + rep
-        
-        print NewUrl
-        print self.host
-        print url
-        
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0',
-                   'Host' : self.host,
-                   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                   'Referer': url,
-                   'Content-Type': 'text/html; charset=utf-8'}
-                   
-        req = urllib2.Request(NewUrl,None,headers)
-        try:
-            response = urllib2.urlopen(req)
-        except urllib2.URLError, e:
-            if e.code == 503:
-            #print e.code
-                #On recupere les fameaux cookies
-                cookies = e.headers['Set-Cookie']
-                cookies = cookies.split(';')[0]
-                #print cookies
-
-        #on refait la requete mais avec les cookies
-        headers.update({'Cookie': cookies})
-        
-        print headers
-        
-        req = urllib2.Request(url,None,headers)
-        try:
-            response = urllib2.urlopen(req)
-            sHtmlContent = response.read()
-            
+            response = opener.open(url)
+            htmlcontent = response.read()
             response.close()
-        except urllib2.URLError, e:
-            print "CloudFlare bypass failed with cookies"
-            print e.code
-            #print e.read()
-        
-            return
+            
+            #print htmlcontent
+            
+            #2 eme etape recuperation cookies
+            hash = re.findall('<input type="hidden" name="jschl_vc" value="(.+?)"\/>',htmlcontent)[0]
+            passe = re.findall('<input type="hidden" name="pass" value="(.+?)"\/>',htmlcontent)[0]
 
+            #calcul de la reponse
+            rep = self.GetResponse(htmlcontent)
+
+            xbmc.sleep(5000)
+            
+            NewUrl = self.hostComplet + '/cdn-cgi/l/chk_jschl?jschl_vc='+ urllib.quote_plus(hash) +'&pass=' + urllib.quote_plus(passe) + '&jschl_answer=' + rep
+            
+            opener = urllib2.build_opener(NoRedirection)
+            opener.addheaders = self.headers
+            #opener.addheaders.append(('Cookie', self.cookies))
+            
+            response = opener.open(NewUrl)
+
+            if 'Set-Cookie' in response.headers:
+                cookies = str(response.headers.get('Set-Cookie'))
+                c1 = re.findall('__cfduid=(.+?);',cookies)
+                c2 = re.findall('cf_clearance=(.+?);',cookies)
+                if not c1 or not c2:
+                    return False
+                cookies = '__cfduid=' + c1[0] + '; cf_clearance=' + c2[0]
+                response.close()
+            else:
+                print "Cookies manquants"
+                response.close()
+                return False
+                
+            #Memorisation
+            print cookies
+            self.SaveCookie(self.host.replace('.','_'),cookies)
+
+
+        #3 eme etape : on refait la requete mais avec les nouveaux cookies 
+        opener = urllib2.build_opener(NoRedirection)
+        opener.addheaders = self.headers
+        
+        #on rajoute les nouveaux coockies
+        opener.addheaders.append (('Cookie', cookies))
+        
+        response = opener.open(url)
+        htmlcontent = response.read()
+        if self.check(htmlcontent):
+            #probleme, cookies plus valide, on l'efface, sera remit a jour la prochaine fois
+            print "Cookies Out of date"
+            self.DeleteCookie(self.host.replace('.','_'))
+            return False
+            
+        response.close()
+            
+        #print htmlcontent
+        
         fh = open('c:\\test.txt', "w")
-        fh.write(sHtmlContent)
+        fh.write(htmlcontent)
         fh.close()
+        
+        return htmlcontent
