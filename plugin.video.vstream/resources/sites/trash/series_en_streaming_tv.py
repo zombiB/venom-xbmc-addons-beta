7440588f98b162e50fa28a0de1d3381ec0da4dc6
TmpName <temporary.asdsd@moncourrier.fr.nf>
2015-10-23 19:37:00 +0200

    Update series_en_streaming_tv.py

--- plugin.video.vstream/resources/sites/trash/series_en_streaming_tv.py
+++ plugin.video.vstream/resources/sites/trash/series_en_streaming_tv.py
@@ -28,27 +28,6 @@ SERIE_SERIES = ('http://www.series-en-streaming.tv/liste/', 'AlphaSearch')
 URL_SEARCH = (URL_MAIN + 'search/', 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
 
-def unescape(text):
-    def fixup(m):
-        text = m.group(0)
-        if text[:2] == "&#":
-            # character reference
-            try:
-                if text[:3] == "&#x":
-                    return unichr(int(text[3:-1], 16))
-                else:
-                    return unichr(int(text[2:-1]))
-            except ValueError:
-                pass
-        else:
-            # named entity
-            try:
-                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-            except KeyError:
-                pass
-        return text # leave as is
-    return re.sub("&#?\w+;", fixup, text)
-
 def load():
     oGui = cGui()
  
@@ -62,7 +41,7 @@ def load():
    
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', SERIE_SERIES[0])
-    oGui.addDir(SITE_IDENTIFIER, SERIE_SERIES[1], 'Séries Liste complete', 'series.png', oOutputParameterHandler)
+    oGui.addDir(SITE_IDENTIFIER, SERIE_SERIES[1], 'Series Liste complete', 'series.png', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
 
